845c72443a23678b69894ad1fbb04c2ebe08b8b3
義陽 馬瀬 <inuinuwanwan0523@outlook.jp>
2022-06-29 09:05:17 +0900

    ルートディレクトリ変更

---
+++ test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            string exeName = @"D:\Debug\WORKAHOLISM.exe";
+            string args = "";
+            ProcessStartInfo startInfo = new ProcessStartInfo(exeName, args)
+            {
+                Verb = "Runas"
+            };
+            Process process = Process.Start(startInfo);
+            process.WaitForExit();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
